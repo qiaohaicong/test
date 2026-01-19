df77f8ad7aae2d6bd71fcb260b3b0180b9fdf314
qiaohaicong <71377437+qiaohaicong@users.noreply.github.com>
2026-01-19 19:07:35 +0800

    Delete src/dddd/caas/sync.sh

--- src/dddd/caas/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nohup python3.9 sync_account_directory.py &
-nohup python3.9 sync_adc_to_local.py &
-nohup python3.9 sync_adc_to_backup.py &
-nohup python3.9 delete_file_new.py &
-nohup python3.9 delete_file.py &
-
